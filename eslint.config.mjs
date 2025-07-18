34dd6c849b28c4c1beb03fc4562c970e91606a34
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-18 09:43:32 +0000

    config: strict eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:@typescript-eslint/recommended"),
 ];
 
 export default eslintConfig;
