0d767891f26c5a743677e032b21f3f662cf697cd
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-18 15:43:51 +0000

    fix: poppins font not loaded on website due to irrelevant variable usage instead of className

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
   return (
     <html lang={lang}>
       <body
-        className={`${poppins.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         {children}
       </body>
