fe870ea4b5436a89fdbeba9793e9cf1528ff5953
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-18 13:39:08 +0000

    config: Move layout.tsx file inside [lang] folder in order to make it default

---
+++ src/app/[lang]/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { i18n } from "@/lib/i18n.config";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Kya Energy",
+  description: "Sustainable Energy Solutions for a Brighter Future",
+};
+
+/*
+ * Generate differents versions of dynamic route [lang]
+ * to avoid page rendering on demand and improve performance
+*/
+export async function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ lang: locale }));
+}
+
+export default async function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ lang: string }>;
+}>) {
+  const { lang } = await params;
+  return (
+    <html lang={lang}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
