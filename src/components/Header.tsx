87d5be4299d3ed5bc33382a7f50d95109e91c0a9
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-18 15:30:36 +0000

    update: remove header css file import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import HeaderMobile from "@/components/HeaderMobile";
 import HeaderDesktop from "@/components/HeaderDesktop";
-import "@/styles/header.css";
 
 export default function Header() {
   return (
