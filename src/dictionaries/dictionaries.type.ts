4572d886ddfa09956159a7c6cc8c2e480150b95e
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-18 13:26:09 +0000

    new: declare type for language dictionaries

---
+++ src/dictionaries/dictionaries.type.ts
@@ -0,0 +1,15 @@
+type Type = {
+  navigation: {
+    home: string,
+    about: string,
+    services: string,
+    contact: string,
+  },
+  hero: {
+    title: string,
+    subtitle: string,
+    cta: string,
+  }
+};
+
+export default Type;
