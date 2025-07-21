8df2587ce70c7ecd9fb1aa398b218ed4ebc5584b
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-21 08:24:24 +0000

    feat: update dark mode button logic to save user preference

--- src/components/ThemeSwitcherButton.tsx
+++ src/components/ThemeSwitcherButton.tsx
@@ -1,42 +1,58 @@
 "use client";
 
-/*
-  * Using context will lead to use
-  * client components everywhere which
-  * is not a good thing for SEO improvement
-*/
 import { useState, useEffect } from "react";
 import { FaMoon, FaSun } from "react-icons/fa6";
 
+type Theme = "light" | "dark";
+
 export default function ThemeSwitcherButton() {
-  const [darkTheme, setDarkTheme] = useState(window.matchMedia("(prefers-color-scheme: dark)").matches); // true if user in dark mode
-  
+  const [theme, setTheme] = useState<Theme | null>(null);
+
+  // Set initial theme from localStorage or system preference on component mount
   useEffect(() => {
-    if(darkTheme) { 
-      if(document.body.classList.contains("dark"))
-        document.body.classList.add("dark")
-      else
-        document.body.classList.remove("dark")
-      }
+    const savedTheme = localStorage.getItem("theme") as Theme | null;
+    const systemPrefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    ).matches;
+
+    if (savedTheme) {
+      setTheme(savedTheme);
+    } else {
+      setTheme(systemPrefersDark ? "dark" : "light");
+    }
   }, []);
-  
+
+  // Update DOM and localStorage whenever theme changes
+  useEffect(() => {
+    if (theme) {
+      if (theme === "dark") {
+        document.documentElement.setAttribute("data-theme", "dark");
+      } else {
+        document.documentElement.setAttribute("data-theme", "light");
+      }
+      localStorage.setItem("theme", theme);
+    }
+  }, [theme]);
+
+  // Function to toggle the theme
   function changeTheme() {
-    setDarkTheme(!darkTheme);
-    document.body.classList.toggle("dark");
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
+  // To prevent a flash of incorrect theme or UI mismatch during server rendering,
+  // the button is rendered only after the theme has been determined on the client.
+  if (theme === null) {
+    return null;
   }
 
   return (
     <button
-      title={darkTheme ? "mode clair" : "mode sombre"}
-      onClick={changeTheme}
-    >
-      {
-        darkTheme ? (
-          <FaSun />
-        ) : (
-          <FaMoon />
-        )
+      className="dark:bg-blue-400"
+      title={
+        theme === "light" ? "Passer au mode sombre" : "Passer au mode clair"
       }
+      onClick={changeTheme}>
+      {theme === "light" ? <FaMoon /> : <FaSun />}
     </button>
   );
 }
