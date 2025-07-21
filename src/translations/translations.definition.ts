c72e9ccdb47380482229ef9fd1a09f481e51b4be
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-21 09:34:10 +0000

    i18n: update translations definitons to include theme switcher

--- src/translations/translations.definition.ts
+++ src/translations/translations.definition.ts
@@ -1,15 +1,23 @@
 type Type = {
   navigation: {
-    home: string,
-    about: string,
-    services: string,
-    contact: string,
-  },
+    home: string;
+    about: string;
+    services: string;
+    contact: string;
+  };
   hero: {
-    title: string,
-    subtitle: string,
-    cta: string,
-  }
+    title: string;
+    subtitle: string;
+    cta: string;
+  };
+  theme: {
+    switcher: {
+      title: {
+        light: string;
+        dark: string;
+      };
+    };
+  };
 };
 
 export default Type;
