69c8e5fffed7cf92b63b70d8053e8956d5da97ca
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-22 09:47:49 +0000

    update: remove bottleneck code and refactor it

--- src/components/LocaleSwitcherButton.tsx
+++ src/components/LocaleSwitcherButton.tsx
@@ -1,37 +1,89 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { FaMoon, FaSun } from "react-icons/fa6";
-import { motion, AnimatePresence } from "framer-motion";
-import TranslationsType from "@/translations/translations.definition";
+import { useState, useEffect, useRef } from "react";
 import { usePathname } from "next/navigation";
-import { i18n, type Locale } from "@/lib/i18n.config";
+import Link from "next/link";
+import { i18n } from "@/lib/i18n.config";
+import TranslationsType from "@/translations/translations.definition";
+import { motion, AnimatePresence } from "framer-motion";
+import {
+  getCurrentLocale,
+  redirectedPathName,
+  getFlagUrl,
+} from "@/lib/locale-utils";
+import { FaChevronDown } from "react-icons/fa6";
+import Image from "next/image";
 
 export default function LocaleSwitcherButton({
-  dictionnary,
+  dictionary,
 }: {
-  dictionnary: TranslationsType;
+  dictionary: TranslationsType;
 }) {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const switcherRef = useRef<HTMLDivElement>(null);
 
-  function getCurrentLocale(): Locale {
-    if (!pathname) return i18n.defaultLocale;
-    const segments = pathname.split("/");
-    return (
-      i18n.locales.find((locale) => locale === segments[1]) ||
-      i18n.defaultLocale
-    );
-  }
+  const currentLocale = getCurrentLocale(pathname);
 
-  const currentLocale = getCurrentLocale();
-
-  useEffect(() => {}, []);
+  // Effect to close the dropdown when clicking outside of it
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        switcherRef.current &&
+        !switcherRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
-    <div>
+    <div className="w-max relative" ref={switcherRef}>
       <button
-        aria-haspopup="true"
-        aria-label={dictionnary.switcher.locale.aria}></button>
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex h-12 items-center justify-center gap-2 bg-slate-200 px-4 rounded font-bold font-sans">
+        <Image
+          src={getFlagUrl(currentLocale)}
+          alt={dictionary.switcher.locale[currentLocale]}
+          width={24}
+          height={0}
+        />
+        <p>{dictionary.switcher.locale[currentLocale]}</p>
+        <motion.div animate={{ rotate: isOpen ? 180 : 0 }}>
+          <FaChevronDown />
+        </motion.div>
+      </button>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="flex flex-col absolute w-full">
+            {i18n.locales.map(
+              (locale, index) =>
+                locale != currentLocale && (
+                  <Link
+                    href={redirectedPathName(pathname, locale)}
+                    key={index}
+                    className="font-bold flex items-center gap-2 px-4 h-12 rounded bg-blue-100 hover:bg-blue-200 transition-colors duration-300">
+                    <Image
+                      src={getFlagUrl(locale)}
+                      alt={dictionary.switcher.locale[locale]}
+                      width={24}
+                      height={0}
+                    />
+                    <p>{dictionary.switcher.locale[locale]}</p>
+                  </Link>
+                ),
+            )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
