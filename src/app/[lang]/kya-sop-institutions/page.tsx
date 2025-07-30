9c3b9e52e3fd56465be7e3f6ef49c8dfcc025804
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-30 16:01:10 +0000

    wip: implementing popup

--- src/app/[lang]/kya-sop-institutions/page.tsx
+++ src/app/[lang]/kya-sop-institutions/page.tsx
@@ -1,4 +1,38 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import { LuX, LuArrowRight } from "react-icons/lu";
+
+function Popup({ title, setClicked }: { title: string; setClicked: any; }) {
+  return (
+    <div className="z-[100] fixed top-0 left-0 w-full h-full bg-kya-coffee/10 flex justify-center items-center">
+      <div className="mx-auto px-8 w-full sm:w-96">
+        <form action="" method="POST" className="bg-white">
+          <div className="flex items-center justify-between p-4 font-bold">
+            <h1>{title}</h1>
+            <button className="px-4 rounded-full bg-kya-coffee" onClick={() => setClicked(null)}><LuX /></button>
+          </div>
+          <div>
+            <p>Veuillez entrer votre numéro de téléphone. Notre équipe se chargera de vous contacter.</p>
+            <label htmlFor="phone-number">
+              <p>Numéro de téléphone</p>
+              <div>
+                <input type="text" name="phone-number" id="phone-number" pattern="^\d*$" required />
+              </div>
+            </label>
+            <button className="w-full">
+              <div className="flex px-8 justify-between items-center group font-bold text-xl bg-kya-green py-4">
+                <p>Contactez-moi</p>
+                <LuArrowRight className="-translate-x-1.5 group-hover:translate-x-1.5 transition-all duration-300" />
+              </div>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default function KyaSopInstitutions() {
   const kyaSopList = [
@@ -58,9 +92,11 @@ export default function KyaSopInstitutions() {
       buyHref: "mailto:marketing@kya-energy.com?subject=ACHAT DE KYA-SoP B30",
     },
   ];
+  const [clicked, setClicked] = useState<number | null>(null);
 
   return (
     <div className="my-16">
+      {clicked != null && (<Popup title={kyaSopList[clicked].title} setClicked={setClicked} />)}
       {
         kyaSopList.map((value, index, array) => (
           <div key={index} className="container mx-auto px-8">
@@ -75,9 +111,9 @@ export default function KyaSopInstitutions() {
                     <li key={index}>{value}</li>
                   ))}
                 </ul>
-                <a href={value.buyHref}>
+                <button onClick={() => setClicked(index)}>
                   <div className="bg-kya-orange p-4 w-max hover:-translate-0.5 font-bold text-kya-white">Demander un devis</div>
-                </a>
+                </button>
               </div>
             </div>
             {/* If not last, display a line under it */}
