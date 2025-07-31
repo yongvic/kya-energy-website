4f4b16423af340ce459540940a1e9dfe1fd2e69f
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-07-31 14:57:32 +0000

    new: redesign header to fit in a single file

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -146,7 +146,7 @@ export default async function RootLayout({
       <body
         className={`bg-gray-100 dark:bg-gray-900 ${libreFranklin.variable} ${libreFranklin.className} ${facebookSans.variable}  ${arialNarrow.variable} ${timesNewRoman.variable} antialiased`}>
         <Header dictionary={dictionary} />
-        <main className="min-h-screen mt-26 md:mt-36">{children}</main>
+        <main className="min-h-screen">{children}</main>
         <Footer dictionary={dictionary} />
       </body>
     </html>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,95 @@
+"use client";
+
 import TranslationsType from "@/translations/translations.definition";
-import HeaderMobile from "@/components/HeaderMobile";
-import HeaderDesktop from "@/components/HeaderDesktop";
+import Image from "next/image";
+import Link from "next/link";
+import ThemeSwitcherButton from "@/components/ThemeSwitcherButton";
+import LocaleSwitcherButton from "@/components/LocaleSwitcherButton";
+import { useState } from "react";
+import { LuMenu, LuX } from "react-icons/lu";
+import { AnimatePresence, motion } from "framer-motion";
+import { Navlink } from "@/components/Navlink";
+import NavbarDesktop from "./NavbarDesktop";
 
 export default function Header({
   dictionary,
 }: {
   dictionary: TranslationsType;
 }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="fixed top-0 left-0 w-full z-40 bg-[#f3f4f60a] dark:bg-[#1018280a] backdrop-blur-xs">
-      <HeaderMobile dictionary={dictionary} className="md:hidden" />
-      <HeaderDesktop dictionary={dictionary} className="hidden md:block" />
+    <header className="sticky top-0 left-0 w-full z-40 bg-[#f3f4f60a] dark:bg-[#1018280a] backdrop-blur-xs">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <div>
+          <Image
+            width={96}
+            height={96}
+            src="/logo.webp"
+            alt={dictionary.header.logo} />
+        </div>
+        <div className="flex items-center justify-center gap-2">
+          <Link href="" className="px-4 py-2 font-bold text-kya-green text-lg">{dictionary.header.qualityPolicy}</Link>
+          <Link href="" className="px-4 py-2 text-lg rounded bg-kya-green hover:bg-kya-green-light text-white font-bold transition-colors duration-300">{dictionary.header.contact}</Link>
+          <div className="hidden md:flex items-center">
+            <LocaleSwitcherButton dictionary={dictionary} />
+            <ThemeSwitcherButton dictionary={dictionary} />
+          </div>
+          <button onClick={() => setIsOpen(!isOpen)} className="md:hidden">
+            <AnimatePresence initial={false} mode="wait">
+              <motion.div
+                key={isOpen ? "x" : "menu"}
+                initial={{ rotate: 45, opacity: 0 }}
+                animate={{ rotate: 0, opacity: 1 }}
+                exit={{ rotate: -45, opacity: 0 }}
+                transition={{ duration: 0.3 }}>
+                {isOpen ? <LuX size={32} /> : <LuMenu size={32} />}
+              </motion.div>
+            </AnimatePresence>
+          </button>
+        </div>
+      </div>
+      <div className="hidden md:block"><NavbarDesktop dictionary={dictionary} /></div>
+      {/* Navlinks */}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.nav
+            className="fixed top-0 left-0 z-50 w-screen h-screen bg-gray-100 dark:bg-gray-900"
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%" }}
+            transition={{ type: "tween", ease: "easeInOut", duration: 0.4 }}>
+            <div className="container mx-auto px-4 h-full">
+              <div className="flex justify-end items-center h-24">
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  aria-label="Close menu">
+                  <LuX size={32} />
+                </button>
+              </div>
+              <div className="flex flex-col items-center w-full pt-8">
+                <Navlink href="/" text={dictionary.navigation.home} isMobile />
+                <Navlink
+                  href="/produits-et-services"
+                  text={dictionary.navigation["products-and-services"]}
+                  isMobile>
+                  <p>Sub link 1</p>
+                  <p>Sub link 2</p>
+                </Navlink>
+                <Navlink
+                  href="/a-propos-de-kya"
+                  text={dictionary.navigation.about}
+                  isMobile
+                />
+                <Navlink
+                  href="/actualites-et-engagement"
+                  text={dictionary.navigation["news-and-engagement"]}
+                  isMobile
+                />
+              </div>
+            </div>
+          </motion.nav>
+        )}
+      </AnimatePresence>
     </header>
   );
 }
