aab56e4083a0357978a89837b08f00643651d64a
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-07 11:31:47 +0000

    update: remove html, head, body and main tags from component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,8 +2,6 @@
 // import 'server-only';
 import Link from 'next/link';
 
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
 import { getTranslation } from '@/lib/get-translation';
 import styles from '@/styles/not-found.module.css';
 import '@/styles/globals.css';
@@ -18,27 +16,19 @@ export default async function NotFound({
   const translation = await getTranslation(lang ?? "fr");
 
   return (
-    <html lang={lang}>
-      <body>
-        <Header dictionary={translation} />
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 dark:bg-gray-900">
-          <div className={styles.container}>
-            <div className={styles.errorCode}>404</div>
-            <div className={styles.errorMessage}>
-              {translation.notFound.title}
-            </div>
-            <p className={styles.errorDescription}>
-              {translation.notFound.description}
-            </p>
-            <Link href={`/${lang}`} passHref>
-              <button className={styles.homeButton}>
-                {translation.notFound.homeButton}
-              </button>
-            </Link>
-          </div>
-        </main>
-        <Footer dictionary={translation} />
-      </body>
-    </html>
+    <div className={styles.container}>
+      <div className={styles.errorCode}>404</div>
+      <div className={styles.errorMessage}>
+        {translation.notFound.title}
+      </div>
+      <p className={styles.errorDescription}>
+        {translation.notFound.description}
+      </p>
+      <Link href={`/${lang}`} passHref>
+        <button className={styles.homeButton}>
+          {translation.notFound.homeButton}
+        </button>
+      </Link>
+    </div>
   );
 }
