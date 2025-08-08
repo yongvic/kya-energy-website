4c7c24541cad708aaa31c83e92861ec15b0244a8
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-08 16:04:39 +0000

    new: include metadata generator for certifications page

--- src/app/[lang]/certifications/page.tsx
+++ src/app/[lang]/certifications/page.tsx
@@ -2,6 +2,7 @@ import { getTranslation } from '@/lib/get-translation';
 import { Locale } from '@/lib/i18n.config';
 import { CertificationClientPage } from './CertificationClientPage';
 import '@/styles/certification.css';
+import { Metadata } from 'next';
 
 export default async function CertificationPage({
   params,
@@ -14,4 +15,16 @@ export default async function CertificationPage({
   return <CertificationClientPage t={t} />;
 }
 
+export async function generateMetadata({
+  params
+}: {
+  params: Promise<{ lang: Locale }>
+}): Promise<Metadata> {
+  const { lang } = await params;
+  const dictionary = await getTranslation(lang);
 
+  return {
+    title: `${dictionary.certification.title} • KYA-Energy Group`,
+    description: dictionary.certification.subtitle,
+  };
+}
