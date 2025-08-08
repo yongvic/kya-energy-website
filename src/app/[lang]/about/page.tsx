9494bece7b1ad7357eda8b13d733acf0e9035d70
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-08 14:58:57 +0000

    new: include metadata generator for about page

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -1,6 +1,7 @@
 import { getTranslation } from '@/lib/get-translation';
 import { Locale } from '@/lib/i18n.config';
 import AboutClientPage from './AboutClientPage';
+import type { Metadata } from 'next';
 
 export default async function AboutPage({
   params,
@@ -12,3 +13,16 @@ export default async function AboutPage({
 
   return <AboutClientPage t={t} />;
 }
+
+export async function generateMetadata({
+  params
+}: { params: Promise<{ lang: Locale }> }): Promise<Metadata> {
+  const { lang } = await params;
+  const dictionary = await getTranslation(lang);
+
+  return {
+    title: dictionary.about.metadata.title,
+    description: dictionary.about.metadata.description,
+  };
+}
+
