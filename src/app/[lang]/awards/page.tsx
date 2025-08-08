eabbab7c0a836045da2993f91b4b9568f148b2ab
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-08 15:58:44 +0000

    new: add metadata for awards page

--- src/app/[lang]/awards/page.tsx
+++ src/app/[lang]/awards/page.tsx
@@ -1,6 +1,7 @@
 import { getTranslation } from '@/lib/get-translation';
 import { Locale } from '@/lib/i18n.config';
 import AwardsClientPage from './AwardsClientPage';
+import { Metadata } from 'next';
 
 export default async function AwardsPage({
   params,
@@ -11,4 +12,18 @@ export default async function AwardsPage({
   const t = await getTranslation(lang);
 
   return <AwardsClientPage t={t} />;
+}
+
+export async function generateMetadata({
+  params
+}: {
+  params: Promise<{ lang: Locale }>
+}): Promise<Metadata> {
+  const { lang } = await params;
+  const dictionary = await getTranslation(lang);
+
+  return {
+    title: `${dictionary.actu.title} • KYA-Energy Group`,
+    description: dictionary.actu.description,
+  };
 }
