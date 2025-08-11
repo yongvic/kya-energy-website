7bf4770a1d22168e07054a9fc6c4a76f38cd751a
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-11 08:13:48 +0000

    update: add metadata for team page

--- src/app/[lang]/team/page.tsx
+++ src/app/[lang]/team/page.tsx
@@ -2,8 +2,7 @@ import { getTranslation } from "@/lib/get-translation";
 import { Locale } from "@/lib/i18n.config";
 import TranslationsType from "@/translations/translations.definition";
 import TeamCarousel from "./TeamCarousel";
-
-
+import { Metadata } from "next";
 
 export default async function TeamPage({
   params,
@@ -17,4 +16,18 @@ export default async function TeamPage({
   return (
     <TeamCarousel teamMembers={teamMembers} />
   );
+}
+
+export async function generateMetadata({
+  params
+}: {
+  params: Promise<{ lang: Locale }>
+}): Promise<Metadata> {
+  const { lang } = await params;
+  const dictionary = await getTranslation(lang);
+
+  return {
+    title: dictionary.about.metadata.title,
+    description: dictionary.about.metadata.description,
+  };
 }
