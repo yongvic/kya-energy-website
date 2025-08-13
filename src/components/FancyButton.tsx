f104b731794117dda156d67e51dd16df3319918d
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-13 15:54:35 +0000

    update: fix some styling issue

--- src/components/FancyButton.tsx
+++ src/components/FancyButton.tsx
@@ -15,19 +15,19 @@ export default function FancyButton({
   if (href && href != null) {
     return (
       <>
-      <style>{`.fancy-button { --bg-color: ${bgColor}; }`}</style>
-      <Link href={href} className={`${className} relative fancy-button overflow-hidden flex justify-center items-center`}>
-        <span className={`relative z-2 w-max`}>{children}</span>
-      </Link>
+        <style>{`.fancy-button { --bg-color: ${bgColor}; }`}</style>
+        <Link href={href} className={`${className} relative fancy-button overflow-hidden flex justify-center items-center`}>
+          <span className={`relative z-2 w-max`}>{children}</span>
+        </Link>
       </>
     );
   } else {
     return (
       <>
-      <style>{`.fancy-button { --bg-color: ${bgColor}; }`}</style>
-      <button className={`${className} relative fancy-button overflow-hidden flex items-center justify-center`}>
-        <span className={`relative z-2 w-max`}>{children}</span>
-      </button>
+        <style>{`.fancy-button { --bg-color: ${bgColor}; }`}</style>
+        <button className={`${className} relative fancy-button overflow-hidden flex items-center justify-center`}>
+          <span className={`relative z-2 w-max`}>{children}</span>
+        </button>
       </>
     )
   }
