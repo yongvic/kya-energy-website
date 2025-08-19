829cd005082387e56c3c8332f73a21dff4ce5b8e
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-19 21:41:03 +0000

    update: include youtube video section into homepage

--- src/app/[locale]/(company)/page.tsx
+++ src/app/[locale]/(company)/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/app/components/standalone/company/homepage/hero";
 import Impacts from "@/app/components/standalone/company/homepage/impacts";
 import MissionEtValeurs from "@/app/components/standalone/company/homepage/mission-et-valeurs";
+import YoutubeVideo from "@/app/components/standalone/company/homepage/youtube-video";
 
 export default function Homepage() {
   return (
@@ -8,6 +9,7 @@ export default function Homepage() {
       <Hero />
       <MissionEtValeurs />
       <Impacts />
+      <YoutubeVideo />
     </>
-  )
+  );
 }
