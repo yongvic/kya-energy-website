ce465d010a72fdab661e595f0c4fd3112b8ff4fe
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-19 21:31:22 +0000

    new: create component for why choose kya

---
+++ src/app/components/standalone/company/homepage/pourquoi-kya.tsx
@@ -0,0 +1,52 @@
+import { strapiUrl } from "@/lib/config";
+import { getLocale, getTranslations } from "next-intl/server";
+
+interface IPourquoiKya {
+  titre: string;
+  description: string;
+  icone: string;
+}
+
+async function getPourquoiKya(locale: string): Promise<IPourquoiKya[]> {
+  const request = await fetch(
+    `${strapiUrl}/api/pourquoi-kyas?locale=${locale}`,
+  );
+  const response = await request.json();
+  const result: IPourquoiKya[] = response.data.map((item: IPourquoiKya) => ({
+    titre: item.titre,
+    description: item.description,
+    icone: item.icone,
+  }));
+  return result;
+}
+
+export default async function PourquoiKya() {
+  const locale = await getLocale();
+  const t = await getTranslations("page d'acceuil.pourquoi kya");
+  const pourquois = await getPourquoiKya(locale);
+
+  return (
+    <section className="py-16 px-8">
+      <div className="container mx-auto text-center mb-12">
+        <h2 className="font-bold text-4xl text-kya-coffee mb-2">
+          {t("titre")}
+        </h2>
+        <div className="w-24 h-1 bg-kya-green mx-auto"></div>
+      </div>
+      <div className="container mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {pourquois.map((item, index) => (
+          <div
+            key={index}
+            className="bg-gray-50 p-6 rounded-lg shadow-md text-center flex flex-col items-center"
+          >
+            <div className="text-kya-green mb-4">{item.icone}</div>
+            <h3 className="text-xl font-bold text-kya-coffee mb-2">
+              {item.titre}
+            </h3>
+            <p className="text-gray-600">{item.description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
