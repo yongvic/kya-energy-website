1a472345bedbca28b45d400ed54d945316abc374
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-19 22:13:35 +0000

    new: create a global not found page

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import styles from "@/styles/not-found.module.css";
+import { getTranslations } from "next-intl/server";
+
+export default async function NotFound() {
+  const t = await getTranslations("introuvable");
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.errorCode}>404</div>
+      <div className={styles.errorMessage}>{t("titre")}</div>
+      <p className={styles.errorDescription}>{t("description")}</p>
+      <Link href={`/`} passHref>
+        <button className={styles.homeButton}>{t("retour à l'acceuil")}</button>
+      </Link>
+    </section>
+  );
+}
