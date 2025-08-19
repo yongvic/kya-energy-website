76e75d488fe16acfcf626160f9b8fbf360518c25
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-19 16:14:39 +0000

    new: insert mission and value section inside homepage

--- src/app/[locale]/(company)/page.tsx
+++ src/app/[locale]/(company)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/app/components/standalone/company/homepage/hero";
+import MissionEtValeurs from "@/app/components/standalone/company/homepage/mission-et-valeurs";
 
 export default function Homepage() {
   return (
     <>
       <Hero />
+      <MissionEtValeurs />
     </>
   )
 }
