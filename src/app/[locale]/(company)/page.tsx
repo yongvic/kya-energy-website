8fb6aa01501775e50fcb21355c4d8fe8a45ade44
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-20 14:19:48 +0000

    update: fix all namings

--- src/app/[locale]/(company)/page.tsx
+++ src/app/[locale]/(company)/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "@/components/standalone/company/homepage/hero";
-import Impacts from "@/components/standalone/company/homepage/impacts";
-import MissionEtValeurs from "@/components/standalone/company/homepage/mission-et-valeurs";
-import PourquoiKya from "@/components/standalone/company/homepage/pourquoi-kya";
-import ProduitsPhares from "@/components/standalone/company/homepage/produits-phares";
-import YoutubeVideo from "@/components/standalone/company/homepage/youtube-video";
+import Hero from "@/components/standalone/company/acceuil/hero";
+import Impacts from "@/components/standalone/company/acceuil/impacts";
+import MissionEtValeurs from "@/components/standalone/company/acceuil/mission-et-valeurs";
+import PourquoiKya from "@/components/standalone/company/acceuil/pourquoi-kya";
+import ProduitsPhares from "@/components/standalone/company/acceuil/produits-phares";
+import YoutubeVideo from "@/components/standalone/company/acceuil/youtube-video";
 import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 
--- src/app/[locale]/(company)/recompenses/page.tsx
+++ src/app/[locale]/(company)/recompenses/page.tsx
@@ -1,8 +1,8 @@
-import DomainesDExcellence from "@/components/standalone/company/awards/domaines-dexcellence";
-import Impact from "@/components/standalone/company/awards/impact";
-import Introduction from "@/components/standalone/company/awards/introduction";
-import PrixEtDistinctions from "@/components/standalone/company/awards/prix-et-distinctions";
-import Rejoindre from "@/components/standalone/company/awards/rejoindre";
+import DomainesDExcellence from "@/components/standalone/company/recompenses/domaines-dexcellence";
+import Impact from "@/components/standalone/company/recompenses/impact";
+import Introduction from "@/components/standalone/company/recompenses/introduction";
+import PrixEtDistinctions from "@/components/standalone/company/recompenses/prix-et-distinctions";
+import Rejoindre from "@/components/standalone/company/recompenses/rejoindre";
 
 export default function Awards() {
   return (
