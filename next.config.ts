d243e003b4a81e8b6d27a18922ab95ce00844b1b
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-26 09:14:44 +0000

    update: allow strapi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import {NextConfig} from 'next';
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {};
- 
+
+const nextConfig: NextConfig = {
+  // Add localhost to allowed doamins
+  // cause of local testing of strapi
+  images: {
+    domains: ['localhost'],
+  },
+};
+
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
