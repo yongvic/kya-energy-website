e7c04408e8778fc1a3fa0bd77e6f0dfcb235b42a
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-26 09:13:24 +0000

    config: exclude more file types

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,10 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from '@/i18n/routing';
- 
+import createMiddleware from "next-intl/middleware";
+import { routing } from "@/i18n/routing";
+
 export default createMiddleware(routing);
- 
+
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|logo.webp|sitemap.xml|robots.txt|.+\.avif|.+\.webp|.+\.png|.+\.ttf|.+\.png|.+\.js|.+\.mp4).*)",
+  ],
 };
