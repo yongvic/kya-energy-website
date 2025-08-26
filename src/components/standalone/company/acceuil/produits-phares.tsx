74a36f63e8cd704539871fdfe2c52f15448c1cb3
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-26 11:46:28 +0000

    update: component design

--- src/components/standalone/company/acceuil/produits-phares.tsx
+++ src/components/standalone/company/acceuil/produits-phares.tsx
@@ -2,6 +2,7 @@ import { strapiUrl } from "@/lib/config";
 import { getLocale, getTranslations } from "next-intl/server";
 import Image from "next/image";
 import Link from "next/link";
+import { LuArrowRight } from "react-icons/lu";
 
 interface IProduitPhare {
   image: {
@@ -49,22 +50,31 @@ export default async function ProduitsPhares() {
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {produitsPhares.map((produit) => (
           <Link href={produit.lien_de_redirection} key={produit.titre}>
-            <div className="w-full relative bg-white shadow-lg group z-10">
-              <div className="overflow-hidden">
+            <div className="w-full relative bg-white shadow-lg group z-10 rounded-xl group **:transition-transform duration-500 ease-in-out">
+              <div className="overflow-hidden pt-6">
                 <div className="h-64 w-max mx-auto">
                   <Image
                     src={produit.image.url}
                     alt={produit.titre}
-                    className="h-64 w-auto object-scale-down"
+                    className="h-64 w-auto object-scale-down group-hover:scale-105"
                     width={225}
                     height={321}
                   />
                 </div>
-                <div className="p-6 bg-white z-10">
-                  <h3 className="text-2xl font-bold text-kya-coffee mb-2">
-                    {produit.titre}
-                  </h3>
-                  <p className="text-gray-700">{produit.description}</p>
+                <div className="p-6 z-10 flex items-center justify-between">
+                  <div>
+                    <h3 className="text-2xl font-bold text-kya-coffee mb-2">
+                      {produit.titre}
+                    </h3>
+                    <p className="text-gray-700">{produit.description}</p>
+                  </div>
+                  <div>
+                    {/* Add a subtle distinction */}
+                    <button className="flex items-center gap-2 text-kya-green font-bold group/button cursor-pointer">
+                      {t("savoir plus")}
+                      <LuArrowRight className="-translate-x-0.5 group-hover/button:translate-x-0" />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
