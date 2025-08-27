8111add1c8bd6c06ef953db8cf1416528b5bb11f
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-27 18:36:47 +0000

    config: configure next-intl

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,27 @@
+import { notFound } from "next/navigation";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import type { ReactNode } from "react";
+import { routing } from "@/i18n/routing";
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: Promise<{
+    locale: string;
+  }>;
+}) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/i18n/routing.ts
@@ -0,0 +1,9 @@
+import { defineRouting } from "next-intl/routing";
+
+export const routing = defineRouting({
+  locales: [
+    "fr",
+    "en",
+  ],
+  defaultLocale: "fr",
+});
