6ca040668fea99cf11ceb931fe1011d26c8980b8
K. Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-28 20:18:12 +0000

    chore: delete everything in old repo

--- src/lib/config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-  strapiApiKey: "1473bbea502af61e9ad805afebb4fc56686248bfce877c59ea5852c784e9243c0bb108bb935cb5306b5d23d6bd1eed6eb720c135fefe9850e8b5b9def9d5d4bdfab632dfcaed0f0bc1c624b04f3237134fe5ee066b03838c3ee7aa8fdb3de322cc9f968fcb2c0dcf7b69568bf20856eaeb07865b84dd9732a683d183bafba4f2",
-  strapiUrl: "http://localhost:1337", // No trailing slash
-  aiWebhook: "https://grateful-aphid-nationally.ngrok-free.app/webhook-test/chatbot",
-};
-
-export default config;
